22e2a472a4e355f434d0bd6c31ff575d844f62bf
Nils Fahle <nils.fahle@marlamalde.group>
2023-06-10 00:06:05 +0200

    delete the queue >:(

--- src/main.rs
+++ src/main.rs
@@ -288,7 +288,7 @@ async fn set_queue_song_played(pool: &State<Pool<MySql>>, uuid: &str, api_key: &
     Ok(())
 }
 
-#[delete("/queue.php?<uuid>&<api_key>", format = "json", data = "<song>")]
+#[post("/queue_delete.php?<uuid>&<api_key>", format = "json", data = "<song>")]
 async fn clear_queue(pool: &State<Pool<MySql>>, uuid: &str, api_key: &str, song: Json<QueueClearPayload>) -> Result<(), Status> {
     verify_access_key(uuid, api_key, pool).await?;
 
